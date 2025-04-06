d8651414e84287125e49b7b9aa52c25bb8614df2
SofiaPutrikova <sofiia.putrikova@e-u.edu.ua>
2025-04-06 16:14:44 -0600

    Update hw06.rs

---
+++ src/homework/hw06.rs
@@ -0,0 +1,19 @@
+fn draw_tree(levels: usize) {
+    let max_width = 2 * levels + 1; // Ширина ялинки
+
+    for i in 0..levels {
+        let height = i + 1; // Висота трикутника
+
+        for row in 0..height {
+            let stars = 2 * row + 1;
+            let spaces = (max_width - stars) / 2;
+
+            println!("{}{}", " ".repeat(spaces), "*".repeat(stars));
+        }
+    }
+}
+
+fn main() {
+    let triangles = 5; // Кількість трикутників
+    draw_tree(triangles);
+}
