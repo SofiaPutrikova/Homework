e84bdf2a6e887ab18b8b3b159230c6c6fb21258f
SofiaPutrikova <sofiia.putrikova@e-u.edu.ua>
2025-04-07 07:40:24 -0600

    Update hw05.rs

--- src/homework/hw05.rs
+++ src/homework/hw05.rs
@@ -1,1 +1,31 @@
+fn gcd(mut a: u32, mut b: u32) -> u32 {
+    while b != 0 {
+        let r = a % b;
+        a = b;
+        b = r;
+    }
+    a
+}
 
+#[test]
+fn test() {
+    let data = [
+        ((24, 60), 12),
+        ((15, 9), 3),
+        ((15, 6), 3),
+        ((140, 40), 20),
+        ((24, 16), 8),
+        ((100, 10), 10),
+        ((120, 80), 40),
+        ((80, 120), 40),
+        ((100, 20), 20),
+        ((37, 11), 1),
+        ((120, 90), 30),
+    ];
+
+    for ((a, b), exp) in data.iter() {
+        let result = gcd(*a, *b);
+        println!("gcd({}, {}) = {}", a, b, result);
+        assert_eq!(*exp, result);
+    }
+}
