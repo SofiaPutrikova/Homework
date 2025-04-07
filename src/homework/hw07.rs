09e4c525faea24b722180a3fdf55513dc61d9ad7
SofiaPutrikova <sofiia.putrikova@e-u.edu.ua>
2025-04-07 08:10:20 -0600

    Update hw07.rs

--- src/homework/hw07.rs
+++ src/homework/hw07.rs
@@ -1,1 +1,34 @@
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().to_string()
+            } else {
+                c.to_lowercase().to_string()
+            }
+        })
+        .collect()
+}
 
+#[test]
+fn test() {
+    let data = [
+        ("Hello", "hELLO"),
+        ("Привет", "пРИВЕТ"),
+    ];
+
+    data
+        .iter()
+        .for_each(|(a, b)| {
+            assert_eq!(
+                invert_the_case(a.to_string()),
+                b.to_string()
+            );
+            assert_eq!(
+                invert_the_case(b.to_string()),
+                a.to_string()
+            );
+            println!("invert_the_case({}) = {}", a, invert_the_case(a.to_string()));
+            println!("invert_the_case({}) = {}", b, invert_the_case(b.to_string()));
+        });
+}
